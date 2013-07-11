bd4ef8ecb06b2589bcd0e289b457deb1999bc431
Jodi Baker <bakerjod@gmail.com>
2013-07-11 16:42:48 +0000

    ascii encoding

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var app = express.createServer(express.logger());
 
 
 	app.get('/', function(request, response) {
-  	response.send(buf.toString(String));
+  	response.send(buf.toString('ascii'));
 	});
 
 
