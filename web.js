3df480f5e8047b6dca69e1f9335c89c098427c64
Jodi Baker <bakerjod@gmail.com>
2013-07-11 16:26:01 +0000

    readFileSync update

--- web.js
+++ web.js
@@ -1,14 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-//var buf =new Buffer(256);
-
-//buf = fs.readFile('index.html');
-//console.log(buf);
 
 
 	app.get('/', function(request, response) {
-  	response.send('Hello World!');
+  	response.send(fs.readFileSync('index.hml', String));
 	});
 
 
