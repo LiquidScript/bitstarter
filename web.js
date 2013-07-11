96159d782e28e07922df9d2178e0beeff1afe3e9
Ubuntu <ubuntu@ip-172-31-43-106.us-west-2.compute.internal>
2013-07-11 16:03:00 +0000

    Update web.js to read from file

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buf =new Buffer(256);
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+buf = fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
+
+if (buf)
+{
+	app.get('/', function(request, response) {
+  	response.send(buf.toString('utf-8'));
+	});
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
 });
