016344e0d693583b81ab2adbf9af5224f32ffa8c
Jodi Baker <bakerjod@gmail.com>
2013-07-11 16:39:20 +0000

    buffered

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var fs = require('fs');
+var buf = new Buffer(512);
+
+buf = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 
 	app.get('/', function(request, response) {
-  	response.send(fs.readFileSync('index.html'));
+  	response.send(buf.toString(String));
 	});
 
 
