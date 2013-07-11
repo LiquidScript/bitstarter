31008e1cba8874aa53f397009696d218debb110d
Jodi Baker <bakerjod@gmail.com>
2013-07-11 16:29:46 +0000

    file correction

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 
 	app.get('/', function(request, response) {
-  	response.send(fs.readFileSync('index.hml', String));
+  	response.send(fs.readFileSync('index.html', String));
 	});
 
 
